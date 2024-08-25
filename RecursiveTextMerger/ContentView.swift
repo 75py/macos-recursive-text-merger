ea64c3d4b3e2556b599f53e2ff88624c942c8409
75py <dev.75py@gmail.com>
2024-08-25 16:50:17 +0900

    Add folder selection functionality to ContentView

--- RecursiveTextMerger/ContentView.swift
+++ RecursiveTextMerger/ContentView.swift
@@ -1,14 +1,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selectedFolder: URL?
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button(action: {
+                selectFolder()
+            }) {
+                Text("フォルダを選択")
+            }
+
+            if let folder = selectedFolder {
+                Text("選択されたフォルダ: \(folder.path)")
+                    .padding(.top, 10)
+            }
         }
         .padding()
+    }
+
+    func selectFolder() {
+        let panel = NSOpenPanel()
+        panel.canChooseFiles = false
+        panel.canChooseDirectories = true
+        panel.allowsMultipleSelection = false
+        panel.begin { response in
+            if response == .OK, let url = panel.url {
+                selectedFolder = url
+            }
+        }
     }
 }
 
